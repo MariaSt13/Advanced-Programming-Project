1009b4f4f8ab163f63f05905292d4c3490948ca0
Maria Strelecky <mariast13@gmail.com>
2017-11-29 17:05:52 +0200

    tests

---
+++ test/gtestMain.cpp
@@ -0,0 +1,11 @@
+//
+// Created by maria on 29/11/17.
+//
+
+#include "gtest/gtest.h"
+
+GTEST_API_ int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+}
