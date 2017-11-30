0980affac0e2d85622e03ed76b246d73c6f67a51
Maria Strelecky <mariast13@gmail.com>
2017-11-30 10:41:17 +0200

    .

--- test/AIPlayerTest.cpp
+++ test/AIPlayerTest.cpp
@@ -2,6 +2,7 @@
 #include "../AIPlayer.h"
 #include "../StandardGameLogic.h"
 #include "../ConsoleBoard.h"
+#include "../HumanLocalPlayer.h"
 
 #define ROW_SIZE 8
 #define COL_SIZE 8
@@ -10,9 +11,10 @@ TEST(AIplayerTest, checkChoseCell) {
     Board::disk  d = Board::whiteActor;
     StandardGameLogic l = StandardGameLogic();
     ConsoleBoard b = ConsoleBoard(ROW_SIZE, COL_SIZE);
+    HumanLocalPlayer h = HumanLocalPlayer(b.blackActor);
     Board::disk** array =  b.getArray();
-    
-    array[3][4] = b.blackActor;
+    l.returnValidMoves(&h,&b);
+    l.flipCells(&h, Point(3,4),&b);
     AIPlayer p = AIPlayer(d, &l, &b);
     EXPECT_EQ(p.chooseStep(), Point(3,3));
 
