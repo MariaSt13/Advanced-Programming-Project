7294114c245636554e82550db2b4a10a0bc81744
Maria Strelecky <mariast13@gmail.com>
2017-12-05 23:33:08 +0200

    small add

--- Server/Server.h
+++ Server/Server.h
@@ -1,8 +1,20 @@
-//
-// Created by maria on 05/12/17.
-//
 
 #ifndef EX4_SERVER_H
 #define EX4_SERVER_H
 
+class Server {
+
+public:
+
+    Server(int port);
+    void start();
+    void stop();
+
+private:
+
+    int port;
+    int serverSocket; // the socket's file descriptor
+    void handleClient(int clientSocket);
+    int calc(int arg1, const char op, int arg2) const;
+};
 #endif //EX4_SERVER_H
