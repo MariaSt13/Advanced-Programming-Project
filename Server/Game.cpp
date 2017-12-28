3d02752ee073b1566ea7fc663f6c69d18c185f64
Linoy <linoya995@gmail.com>
2017-12-28 15:54:03 +0200

    jfjfj

---
+++ Server/Game.cpp
@@ -0,0 +1,26 @@
+//
+// Created by linoy on 28/12/17.
+//
+
+#include "Game.h"
+
+/**
+ * constructor
+ * @param name - name of the game
+ * @param socketFirstPLayer - number socket of the first player.
+ */
+Game::Game(string name, int socketFirstPLayer):name(name),socketFirstPLayer(socketFirstPLayer),run(false) {}
+
+
+/**
+ * join second player to the game.
+ * @param socketPLayer - number socket of the second player.
+ */
+void Game::joinToGame(int socketPLayer) {
+    this->socketSecondPLayer = socketPLayer;
+    this->run = true;
+}
+
+const string Game::getName()const {
+    return this->name;
+}
---
+++ Server/Game.h
@@ -0,0 +1,31 @@
+//
+// Created by linoy on 28/12/17.
+//
+
+#ifndef EX4_GAME_H
+#define EX4_GAME_H
+
+#include <string>
+using namespace std;
+
+
+class Game {
+public:
+    //constructor
+    Game(string name,int socketFirstPLayer);
+
+    //join second player to game
+    void joinToGame(int socketPLayer);
+
+    const string getName()const;
+
+private:
+    bool run;
+    string name;
+    int socketFirstPLayer;
+    int socketSecondPLayer;
+
+};
+
+
+#endif //EX4_GAME_H
